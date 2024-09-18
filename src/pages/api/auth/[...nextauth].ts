77ebe69013f2b7228e139a886151482d362e7392
Devanshu Kumar <devanshukumar289@gmail.com>
2024-09-18 14:52:24 +0530

    today1 commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,6 +12,9 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      httpOptions: {
+      timeout: 10000, 
+    },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
