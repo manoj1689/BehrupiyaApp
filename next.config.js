a8aa9d039768cdad5b74e374db27cf5a67c58d34
monty1689 <work.manojkaushik@gmail.com>
2024-09-28 17:07:42 +0530

    chatbot Upgraded

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ["122.160.116.97"],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      encoding: false,
+      fs: false, // Add this line
+    };
+    return config;
+  },
 };
